3ca7c2ade18162e7772945f8bbd61dec6b414d70
Romans S <romans.spilaks@gmail.com>
2023-08-15 23:46:57 +0300

    Add methods for Comment's counter associated with Post to increase or decrease commentaries count by increment! or decrement! actions. Add similar counters to Like (assoc with Post) and Post (assoc with User. Add method to User model to query the recent posts (on created_at by DESC LIMIT 3)

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,19 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  after_create :update_user_posts_counter
+  before_destroy :update_user_posts_counter_before_destroy
+
+  def update_user_posts_counter
+    user.increment!(:posts_counter)
+  end
+
+  def update_user_posts_counter_before_destroy
+    user.decrement!(:posts_counter)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
