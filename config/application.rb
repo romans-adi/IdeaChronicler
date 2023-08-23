e4d471bcf6434032c3064b5b4f6193420142fa8a
Romans S <romans.spilaks@gmail.com>
2023-08-23 09:17:35 +0300

    Add tailwind css to the project. Remove all plain css styles and add tailwind classes. Add localization to the project

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module IdeaChronicler
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :en
+    config.assets.enabled = true
+
   end
 end
