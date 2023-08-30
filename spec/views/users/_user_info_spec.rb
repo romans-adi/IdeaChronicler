a61109975f8520aad406590968709adb1a91beba
Romans S <romans.spilaks@gmail.com>
2023-08-30 03:44:34 +0300

    Group tests for partial user_info and view user_show. Add user_index_spec.

--- spec/views/users/_user_info_spec.rb
+++ spec/views/users/_user_info_spec.rb
@@ -1,20 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature 'User index page', type: :feature do
-  scenario 'displays a list of users with their information' do
-    user1 = User.create(name: 'Vilis', photo: 'abd123', posts_counter: 23)
-    user2 = User.create(name: 'Another Smith', photo: 'abd123', posts_counter: 10)
+  context 'displaying user information' do
+    let!(:user1) { User.create(name: 'Vilis', photo: 'abd123', posts_counter: 23) }
+    let!(:user2) { User.create(name: 'Another Smith', photo: 'abd123', posts_counter: 10) }
 
-    visit users_path
+    before do
+      visit users_path
+    end
 
-    expect(page).to have_content(user1.name)
-    expect(page).to have_link(user1.name, href: user_path(user1))
-    expect(page).to have_content("#{user1.posts_counter} posts")
-    expect(page).to have_link("#{user1.posts_counter} posts", href: user_posts_path(user1))
+    it 'displays usernames and post counts' do
+      expect(page).to have_content(user1.name)
+      expect(page).to have_content(user2.name)
+      expect(page).to have_content("#{user1.posts_counter} posts")
+      expect(page).to have_content("#{user2.posts_counter} posts")
+    end
 
-    expect(page).to have_content(user2.name)
-    expect(page).to have_link(user2.name, href: user_path(user2))
-    expect(page).to have_content("#{user2.posts_counter} posts")
-    expect(page).to have_link("#{user2.posts_counter} posts", href: user_posts_path(user2))
+    it 'provides links to user show pages' do
+      expect(page).to have_link(user1.name, href: user_path(user1))
+      expect(page).to have_link(user2.name, href: user_path(user2))
+    end
+
+    it 'provides links to user post index pages' do
+      expect(page).to have_link("#{user1.posts_counter} posts", href: user_posts_path(user1))
+      expect(page).to have_link("#{user2.posts_counter} posts", href: user_posts_path(user2))
+    end
   end
 end
--- spec/views/users/user_show_spec.rb
+++ spec/views/users/user_show_spec.rb
@@ -2,34 +2,16 @@ require 'rails_helper'
 
 RSpec.feature 'User show page', type: :feature do
   include Rails.application.routes.url_helpers
-  scenario 'displays user details, bio, and recent posts' do
-    user = User.create(name: 'Another Smith', photo: 'abd123', posts_counter: 10)
-    post1 = user.posts.create(
-      author: user,
-      title: 'Hello Post',
-      text: 'Hi',
-      comments_counter: 0,
-      likes_counter: 0
-    )
 
-    post3 = user.posts.create(
-      author: user,
-      title: 'Hello Kost',
-      text: 'Hi Hey',
-      comments_counter: 0,
-      likes_counter: 0
-    )
+  let!(:user) { User.create(name: 'Another Smith', photo: 'abd123', posts_counter: 10) }
 
+  it 'displays user details and recent posts' do
     visit user_path(user)
-
     expect(page).to have_content(user.name)
     expect(page).to have_link(user.name, href: user_path(user))
     expect(page).to have_content("#{user.posts_counter} posts")
     expect(page).to have_link("#{user.posts_counter} posts", href: user_posts_path(user))
     expect(page).to have_content(user.bio.to_s)
-    expect(page).not_to have_content(post1.title)
-    expect(page).not_to have_content(post3.title)
-
     expect(page).to have_link('See All Posts', href: user_posts_path(user))
   end
 end
