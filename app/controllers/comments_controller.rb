cb1929bb840e839714c55b165db99e7dcb3b88c1
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-31 18:27:27 +0300

    Change the dirction of the comment page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,42 +1,36 @@
 class CommentsController < ApplicationController
-  before_action :find_post
-
-  def new
-    @comment = Comment.new
-  end
-
+  load_and_authorize_resource :post
+  load_and_authorize_resource :comment, through: :post
   def create
-    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
     @comment.author = current_user
 
     if @comment.save
       flash[:success] = 'Comment was successfully added!'
+      redirect_to user_post_path(current_user, @post)
     else
       flash.now[:error] = 'Failed to add comment'
+      render 'new'
     end
-    redirect_to user_post_path(params[:user_id], params[:post_id])
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     authorize! :destroy, @comment
+    @comment.destroy
+    @user = current_user
+    redirect_to user_post_path(@user, @post)
+  end
 
-    if @comment.destroy
-      flash[:notice] = "Comment was successfully deleted."
-    else
-      flash[:error] = "Failed to delete the comment."
-    end
-
-    redirect_to root_path
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
   end
 
   private
 
-  def find_post
-    @post = Post.find(params[:post_id])
-  end
-
   def comment_params
-    params.require(:comment).permit(:text, :post_id)
+    params.require(:comment).permit(:text)
   end
 end
