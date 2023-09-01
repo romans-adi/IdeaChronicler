d1a6cec5c38e6b263c61b11b879b9a5957263423
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-09-01 18:31:27 +0300

    Add the secret key to token generator

--- app/helpers/token_generator.rb
+++ app/helpers/token_generator.rb
@@ -1,7 +1,7 @@
 require 'jwt'
 
 class TokenGenerator
-  SECRET_KEY = Rails.application.credentials.fetch(:secret_key_base)
+  SECRET_KEY = "qx+ZnuZqUCXPEyBpq/OscAnbLslex56eC9zAkQ3YezKeOFKaVr6sdEscjUhGtYdzY2O+0b99g2wIw+g9XZbXAWDIy88PW6+euXgxO7QvH8KcTOdWbipFxYBuMmpOO6KZ0DYq6lKjry46Iy5+4J5qVcXH/FNSgL8zC7UhRcR/GwPSbOGPZfrS8vT6nSJZMUrQDKwpeTd1O7lrwKn+DyTkPvBwwFTQfgqxLBQw6Jg54XrRqcD7cebiCM+DpoGTdfgTVPBqeDQDGGBNDW1qmAWSNItknuhB43s16h0I+u9GdD9TCbN32lrRtOggkSRBPUWk/5CXUMVlhW/PoYHqQhly9dxd0I4+JbNBViXxusrJ5tSJZOBp8eYHex803cG5R44929hJRV+bXwfNe0tlOYG9ZmmML8Aqvehd97y/--cg301Ie1Rk2yB7ED--BDMxEnDSqd8qMgoCftj3lQ=="
 
   def self.generate_authentication_token(user_id)
     payload = { user_id: user_id }
